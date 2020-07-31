c29aeb2be0629f90b659834497dba2c5556d5183
Kegan Heiss <whisconierhillgames@gmail.com>
2020-07-31 10:08:48 -0400

    First commit
    Particle steam and 'filter' collider are set up.  Particles are filtered out by the trigger module / custom script, but trails are not.  Goal is for trails to disappear when intersecting with the collider.

---
+++ Assets/Scripts/ParticleFilterTrigger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class ParticleFilterTrigger : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnEnable()
+    {
+
+    }
+
+    void OnParticleTrigger()
+    {
+        // get the particles which matched the trigger conditions this frame
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+        // iterate through the particles which entered the trigger and make them red
+        for (int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle p = enter[i];
+            p.startColor = new Color32(0, 0, 0, 0);
+            enter[i] = p;
+        }
+
+        // iterate through the particles which exited the trigger and make them green
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p = exit[i];
+            p.startColor = new Color32(255, 255, 255, 255);
+            exit[i] = p;
+        }
+
+        // re-assign the modified particles back into the particle system
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+    }
+}
